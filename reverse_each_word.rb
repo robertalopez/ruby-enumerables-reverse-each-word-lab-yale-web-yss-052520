7318430f9002558823c4d83a6786d373e03a29b5
Robert Lopez <robert.lopez@yale.edu>
2020-05-15 00:01:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   yarra = []
   new_array = string.split(/ /)
-  new_array.collects do |word|
+  new_array.collect do |word|
    yarra << word.reverse
  end 
    yarra.join(" ")
